9e2b210aafba723be926b03f7073b91dd6a48ad5
Alonso Robles <alonso@alonsorobles.com>
2018-01-08 09:48:41 -0600

    http to express

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import http from 'http';
+import express from 'express';
 
-http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'text/plain'
-    });
-    res.end('Hello World\n');
-}).listen(3000, '127.0.0.1');
+const app = express();
 
-console.log('Server running at http://127.0.0.1:3000/');
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
 
+app.listen(3000, () => {
+    console.log('Server running at http://127.0.0.1:3000/');
+});
