6efae67a9dec8bd9f9fcb51dfadc0f7aed2391b1
Alonso Robles <alonso@alonsorobles.com>
2018-01-08 13:01:23 -0600

    Added ping

--- routes/pet.js
+++ routes/pet.js
@@ -1,28 +1,47 @@
-import r from 'request';
+import request from 'request';
 import async from 'async';
 
-r.defaults({json: true});
+const r = request.defaults({json: true});
 
 const pets = app => {
 
   // Read
   app.get('/pets', (req, res) => {
 
-    r({uri: 'http://localhost:3001/dog'}, (error, response, body) => {
-      if (!error && response.statusCode === 200) {
-        res.json(body);
-      } else {
-        res.send(response.statusCode);
-      }
+    async.parallel({
+      cat: callback => {
+        r({uri: 'http://localhost:3000/cat'}, (error, response, body) => {
+          if (error) {
+            callback({service: 'cat', error});
+          }
+          if (!error && response.statusCode === 200) {
+            callback(null, body.data);
+          } else {
+            callback(response.statusCode);
+          }
+        })
+      },
+      dog: callback => {
+        r({uri: 'http://localhost:3001/dog'}, (error, response, body) => {
+          if (error) {
+            callback({service: 'dog', error});
+          }
+          if (!error && response.statusCode === 200) {
+            callback(null, body.data);
+          } else {
+            callback(response.statusCode);
+          }
+        })
+      },
+    }, (error, results) => {
+      res.json({error, results});
     });
 
-    r({uri: 'http://localhost:3000/cat'}, (error, response, body) => {
-      if (!error && response.statusCode === 200) {
-        res.json(body);
-      } else {
-        res.send(response.statusCode);
-      }
-    });
+  });
+
+  app.get('/ping', (req, res) => {
+
+    res.json({pong: Date.now()});
 
   });
 
